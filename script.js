bccc2cd0bd1c06e482c82c3ed0a00b82f5609c5d
Luiz Felipe <luizfspereira@outlook.com>
2021-07-21 17:01:20 -0300

    Definindo choque da cobrinha

--- script.js
+++ script.js
@@ -43,6 +43,13 @@ function iniciarJogo(){
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    for(i=1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("Game Over!")
+        }
+    }
+
     //Chama as funções de criar background e cobrinha ao iniciar o jogo
     criarBG();
     criarCobrinha();
@@ -64,7 +71,6 @@ function iniciarJogo(){
         food.x = Math.floor(Math.random() * 15 + 1) * box,
         food.y = Math.floor(Math.random() * 15 + 1) * box
     }
-
     
     let newHead = { //Define onde a cabeça da cobra será criada
         x: snakeX,
